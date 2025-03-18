3844fd72fec78aab73f60efe4538b16c9f0c46f8
Stefnotch <brandmairstefan@gmail.com>
2025-03-18 15:45:53 +0100

    I can't get the wesl.toml to work

--- wesl-sample-vite/vite.config.ts
+++ wesl-sample-vite/vite.config.ts
@@ -2,7 +2,9 @@ import viteWesl from "wesl-plugin/vite";
 import { linkBuildExtension } from "wesl-plugin";
 
 const config = {
-  plugins: [viteWesl({ extensions: [linkBuildExtension] })],
+  plugins: [
+    viteWesl({ extensions: [linkBuildExtension], weslToml: "./wesl.toml" }),
+  ],
 };
 
 export default config;
